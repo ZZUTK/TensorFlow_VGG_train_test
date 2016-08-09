ba2742fd9093bcd29455bebb3934afdc8169eee4
ZZUTK <zzf861011@gmail.com>
2016-08-09 17:20:04 -0400

    Add files via upload

--- training.py
+++ training.py
@@ -3,11 +3,13 @@
 # written by Zhifei Zhang, Aug., 2016
 # Details: https://github.com/ZZUTK/TensorFlow_VGG_train_test
 #
-# This is a demo of training process using CIFAR-10 dataset
+# This is a demo of training process on CIFAR-10 dataset.
+# Because VGG16 model is too large to quickly get some result, we use a shallow model
+# (2 convolution + 1 fully connection), which is also named vgg16. but it is NOT VGG16 model.
 #####################################################################################################
 
-# from VGG16_model import vgg16
 from shallow_model import vgg16
+# from VGG16_model import vgg16
 import tensorflow as tf
 import numpy as np
 
@@ -17,7 +19,7 @@ import numpy as np
 # valid_x is a 4-D matrix like train_x
 # valid_y is a 2-D matrix like train_y
 def training(train_x, train_y, valid_x=None, valid_y=None, format_size=[224, 224],
-             batch_size=10, learn_rate=0.01, num_epochs=1, save_model=False):
+             batch_size=10, learn_rate=0.01, num_epochs=1, save_model=False, debug=False):
     
     assert len(train_x.shape) == 4
     [num_images, img_height, img_width, num_channels] = train_x.shape
@@ -32,7 +34,7 @@ def training(train_x, train_y, valid_x=None, valid_y=None, format_size=[224, 224
         train_maps = tf.image.resize_images(train_maps_raw, format_size[0], format_size[1])
         train_labels = tf.placeholder(tf.float32, [None, num_classes])
         # logits, parameters = vgg16(train_maps, num_classes)
-        logits = vgg16(train_maps, num_classes, isTrain=True, keep_prob=1.0)
+        logits = vgg16(train_maps, num_classes, isTrain=True, keep_prob=0.6)
 
         # loss function
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, train_labels)
@@ -60,18 +62,17 @@ def training(train_x, train_y, valid_x=None, valid_y=None, format_size=[224, 224
                 feed_dict = {train_maps_raw: batch_data, train_labels: batch_labels}
                 _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
 
-                # print for debug
-                # if step % int(np.ceil(num_steps/2.0)) == 0:
-                #     print('Epoch %2d/%2d step %2d/%2d: ' % (epoch, num_epochs, step, num_steps))
-                #     print('\tBatch Loss = %.2f\t Accuracy = %.2f%%' % (l, accuracy(predictions, batch_labels)))
-                #     if valid_x is not None:
-                #         feed_dict = {train_maps_raw: valid_x, train_labels: valid_y}
-                #         l, predictions = session.run([loss, train_prediction], feed_dict=feed_dict)
-                #         print('\tValid Loss = %.2f\t Accuracy = %.2f%%' % (l, accuracy(predictions, valid_y)))
-                #     print(' ')
+                if debug:
+                    if step % int(np.ceil(num_steps/2.0)) == 0:
+                        print('Epoch %2d/%2d step %2d/%2d: ' % (epoch+1, num_epochs, step, num_steps))
+                        print('\tBatch Loss = %.2f\t Accuracy = %.2f%%' % (l, accuracy(predictions, batch_labels)))
+                        if valid_x is not None:
+                            feed_dict = {train_maps_raw: valid_x, train_labels: valid_y}
+                            l, predictions = session.run([loss, train_prediction], feed_dict=feed_dict)
+                            print('\tValid Loss = %.2f\t Accuracy = %.2f%%' % (l, accuracy(predictions, valid_y)))
 
             print ('Epoch %2d/%2d:\n\tTrain Loss = %.2f\t Accuracy = %.2f%%' %
-                   (epoch, num_epochs, l, accuracy(predictions, batch_labels)))
+                   (epoch+1, num_epochs, l, accuracy(predictions, batch_labels)))
             if valid_x is not None and valid_y is not None:
                 feed_dict = {train_maps_raw: valid_x, train_labels: valid_y}
                 l, predictions = session.run([loss, train_prediction], feed_dict=feed_dict)
@@ -105,8 +106,6 @@ if __name__ == '__main__':
     # std = np.std(tr_x, axis=(0, 1, 2))
     print('Color center of images: %s' % mu )
     tr_x[:, :, :, :] -= mu.reshape(1, 1, 1, 3)
-    # tr_x[:, :, :, :] /= std.reshape(1, 1, 1, 3)
-
     te_x[:, :, :, :] -= mu.reshape(1, 1, 1, 3)
 
     # one-hot labels [num_images, num_classes]
@@ -117,8 +116,8 @@ if __name__ == '__main__':
     for i in range(len(te_y)):
         te_y_onehot[i][te_y[i]] = 1.0
 
-    # training
+    # training on a subset to get a quick result
     print('Training ...')
     training(tr_x[::10, :, :, :], tr_y_onehot[::10, :],
              te_x[::10, :, :, :], te_y_onehot[::10, :],
-             format_size=[32, 32], batch_size=50, learn_rate=0.1, num_epochs=30)
+             format_size=[32, 32], batch_size=50, learn_rate=0.1, num_epochs=80)
